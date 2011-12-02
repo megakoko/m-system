f53055a1a5806b9c8bb55623c341420e45663506
megadestroyer <megadestroyer@5efcf63a-43f3-4715-9d6f-55a9d6060040>
2011-12-02 16:13:22 +0000

    Добавил построение дерева по мкб10.

--- src/mkb10/mainmkb10widget.cpp
+++ src/mkb10/mainmkb10widget.cpp
@@ -1,9 +1,104 @@
 #include "mainmkb10widget.h"
+
+
 #include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlError>
+#include <QDebug>
+
+#include "macros.h"
+#include "mkb10treemodel.h"
 
 
 MainMkb10Widget::MainMkb10Widget(QWidget *parent)
 	: PluginWidget(parent)
 {
 	setupUi(this);
+
+	initTreeWidget();
+
+	connect(treeWidget, SIGNAL(itemExpanded(QTreeWidgetItem*)),
+			SLOT(treeWidgetItemExpanded(QTreeWidgetItem*)));
+}
+
+
+void MainMkb10Widget::initTreeWidget()
+{
+	QSqlQuery q("SELECT id, description FROM mkb10 WHERE parentId IS NULL ORDER BY id");
+	checkQuery(q);
+
+	QList<QTreeWidgetItem*> topLevelItems;
+	while(q.next())
+		topLevelItems << createTreeWidgetItem(q.record());
+
+	treeWidget->addTopLevelItems(topLevelItems);
+}
+
+
+void MainMkb10Widget::treeWidgetItemExpanded(QTreeWidgetItem *item)
+{
+	if(itemHasDummyChildItem(item))
+	{
+		removeDummyChildItem(item);
+
+		QSqlQuery q;
+		q.prepare("SELECT id, description FROM mkb10 WHERE parentId = :parentId ORDER BY id");
+		q.bindValue(":parentId", item->data(0, Qt::UserRole));
+		q.exec();
+		checkQuery(q);
+
+		QList<QTreeWidgetItem*> children;
+		while(q.next())
+			children << createTreeWidgetItem(q.record());
+
+		item->addChildren(children);
+	}
+}
+
+
+
+QTreeWidgetItem* MainMkb10Widget::createTreeWidgetItem(const QSqlRecord& rec)
+{
+	QTreeWidgetItem* item = new QTreeWidgetItem;
+
+	const int id = rec.value("id").toInt();
+	item->setData(0, Qt::UserRole, id);
+	item->setText(0, rec.value("description").toString());
+
+	if(itemHasChildren(id))
+		addDummyChildItem(item);
+
+	return item;
+}
+
+
+void MainMkb10Widget::addDummyChildItem(QTreeWidgetItem* item)
+{
+	item->addChild(new QTreeWidgetItem);
+}
+
+
+void MainMkb10Widget::removeDummyChildItem(QTreeWidgetItem *item)
+{
+	Q_ASSERT(itemHasDummyChildItem(item));
+	item->removeChild(item->child(0));
+}
+
+
+bool MainMkb10Widget::itemHasDummyChildItem(const QTreeWidgetItem* item)
+{
+	return (item->childCount() == 1 && item->child(0)->data(0, Qt::UserRole).isNull());
+}
+
+
+bool MainMkb10Widget::itemHasChildren(const int id)
+{
+	QSqlQuery q;
+	q.prepare("SELECT COUNT(*) FROM mkb10 WHERE parentId = :id");
+	q.addBindValue(id);
+	q.exec();
+	checkQuery(q);
+	q.first();
+
+	return (q.value(0).toInt() > 0);
 }
--- src/mkb10/mainmkb10widget.h
+++ src/mkb10/mainmkb10widget.h
@@ -3,10 +3,27 @@
 #include "pluginwidget.h"
 #include "ui_mainmkb10widget.h"
 
+
+class QSqlRecord;
+
 class MainMkb10Widget : public PluginWidget, private Ui::MainMkb10Widget
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit MainMkb10Widget(QWidget *parent = 0);
+	explicit MainMkb10Widget(QWidget *parent = 0);
+
+private:
+	static QTreeWidgetItem* createTreeWidgetItem(const QSqlRecord& rec);
+
+	static void addDummyChildItem(QTreeWidgetItem* item);
+	static void removeDummyChildItem(QTreeWidgetItem* item);
+	static bool itemHasDummyChildItem(const QTreeWidgetItem* item);
+
+	static bool itemHasChildren(const int id);
+
+
+private slots:
+	void initTreeWidget();
+	void treeWidgetItemExpanded(QTreeWidgetItem* item);
 };
