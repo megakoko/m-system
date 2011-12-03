e1865109be878639c3502a3901987ab318347ac3
megadestroyer <megadestroyer@5efcf63a-43f3-4715-9d6f-55a9d6060040>
2011-12-03 14:15:44 +0000

    Добавил таблицы для модуля информации о мед. учреждении. Убрал NOTICE-сообщения, генерируемые при запуске скрипта создания таблиц БД.

--- src/resources/dbinit.sql
+++ src/resources/dbinit.sql
@@ -1,11 +1,20 @@
+SET client_min_messages = 'warning';
+
+
 ---------------------------------------------------------------------
 ----------------------- Dropping tables -----------------------------
 
 
+DROP TABLE IF EXISTS DepartmentStaffPosition;
+DROP TABLE IF EXISTS Position;
+DROP TABLE IF EXISTS Department;
+DROP TABLE IF EXISTS DepartmentType;
+DROP TABLE IF EXISTS Staff;
+DROP TABLE IF EXISTS HealthFacility;
 DROP TABLE IF EXISTS MKB10;
 DROP TABLE IF EXISTS Address;
 DROP TABLE IF EXISTS Document;
-DROP TABLE IF EXISTS DocumentType; 
+DROP TABLE IF EXISTS DocumentType;
 DROP TABLE IF EXISTS Patient;
 DROP TABLE IF EXISTS UserPluginAccess;
 DROP TABLE IF EXISTS Plugins;
@@ -30,7 +39,7 @@ CREATE TABLE Plugins (
 );
 
 CREATE TABLE UserPluginAccess (
-	id 					SERIAL,
+	id 					SERIAL PRIMARY KEY,
 	userid 				INTEGER REFERENCES MUser (id),
 	pluginid 			INTEGER REFERENCES Plugins (id)
 );
@@ -45,8 +54,8 @@ CREATE TABLE Patient (
 );
 
 CREATE TABLE DocumentType (
-	id 					SERIAL PRIMARY KEY, 
-	textid 				VARCHAR, 
+	id 					SERIAL PRIMARY KEY,
+	textid 				VARCHAR,
 	name		 		VARCHAR
 );
 
@@ -65,7 +74,7 @@ CREATE TABLE Address (
 	isMailingAddress 	BOOL NOT NULL,		-- Является адресом по прописке.
 	city 				VARCHAR NOT NULL,
 	street 				VARCHAR NOT NULL,
-	house 				VARCHAR NOT NULL, 
+	house 				VARCHAR NOT NULL,
 	apartment 			VARCHAR
 );
 
@@ -76,21 +85,71 @@ CREATE TABLE MKB10 (
 	description			VARCHAR,
 	parentId			INTEGER REFERENCES MKB10(id)
 );
- 
+
+
+
+CREATE TABLE HealthFacility (
+	name				VARCHAR,
+	shortName			VARCHAR
+
+);
+
+
+CREATE TABLE Staff (
+	id					SERIAL PRIMARY KEY,
+	familyName 			VARCHAR,
+	name 				VARCHAR,
+	patronymic 			VARCHAR,
+	birthDate			TIMESTAMP,
+	specialization		VARCHAR
+);
+
+
+CREATE TABLE DepartmentType (
+	id					SERIAL PRIMARY KEY,
+	name				VARCHAR
+);
+
+
+CREATE TABLE Department (
+	id					SERIAL PRIMARY KEY,
+	name				VARCHAR,
+	shortName			VARCHAR,
+	typeId				INTEGER REFERENCES DepartmentType(id),
+	headOfDepartmentId	INTEGER REFERENCES Staff(id)
+);
+
+
+CREATE TABLE Position (
+	id					SERIAL PRIMARY KEY,
+	name				VARCHAR
+);
+
+
+CREATE TABLE DepartmentStaffPosition (
+	departmentId		INTEGER REFERENCES Department(id),
+	staffId				INTEGER REFERENCES Staff(id),
+	positionId			INTEGER REFERENCES Position(id)
+);
+
 
 ---------------------------------------------------------------------
 ----------------------------- Data ----------------------------------
 -- Admin user with login 'admin' and password 'pw'.
-INSERT INTO MUser(login, password, salt, is_admin) VALUES 
+INSERT INTO MUser(login, password, salt, is_admin) VALUES
 ('admin', '38b311d8c359e5975c5a3f454d3f4294', 'salt', true);
 
 
 INSERT INTO Plugins(textid, name) VALUES
-('admin', 'Администратор'), 
+('admin', 'Администратор'),
 ('patients', 'Пациенты'),
 ('mkb10', 'Справочная система МКБ-10');
 
 
 INSERT INTO DocumentType(textid, name) VALUES
-('passport', 'Паспорт'), 
+('passport', 'Паспорт'),
 ('taxpayerId', 'ИНН');
+
+
+---------------------------------------------------------------------
+SET client_min_messages = 'notice';
