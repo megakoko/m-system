e66e91e79b2908dabea1d61d35e94332c55c69ac
megadestroyer <megadestroyer@5efcf63a-43f3-4715-9d6f-55a9d6060040>
2011-12-24 12:42:52 +0000

    Изменил способ добавления персонала.

--- msystem/src/departments/staffeditwidget.cpp
+++ msystem/src/departments/staffeditwidget.cpp
@@ -22,22 +22,26 @@ void StaffEditWidget::init()
 {
 	Q_ASSERT(m_staffId > 0);
 
-	QSqlQuery q;
-	q.prepare(" SELECT familyName, name, patronymic, birthday, specialization "
-			  " FROM Staff "
-			  " WHERE id = :id ");
-	q.addBindValue(m_staffId);
-	q.exec();
-	checkQuery(q);
 
-	const bool idIsValid = q.first();
-	Q_ASSERT(idIsValid); Q_UNUSED(idIsValid);
+	if(m_staffId != InvalidId)
+	{
+		QSqlQuery q;
+		q.prepare(" SELECT familyName, name, patronymic, birthday, specialization "
+				  " FROM Staff "
+				  " WHERE id = :id ");
+		q.addBindValue(m_staffId);
+		q.exec();
+		checkQuery(q);
 
-	m_familyName->setText(q.value(0).toString());
-	m_name->setText(q.value(1).toString());
-	m_patronymic->setText(q.value(2).toString());
-	m_birthDay->setDate(q.value(3).toDate());
-	m_specialization->setText(q.value(4).toString());
+		const bool idIsValid = q.first();
+		Q_ASSERT(idIsValid); Q_UNUSED(idIsValid);
+
+		m_familyName->setText(q.value(0).toString());
+		m_name->setText(q.value(1).toString());
+		m_patronymic->setText(q.value(2).toString());
+		m_birthDay->setDate(q.value(3).toDate());
+		m_specialization->setText(q.value(4).toString());
+	}
 }
 
 
@@ -53,10 +57,22 @@ void StaffEditWidget::initConnections()
 
 QString StaffEditWidget::staffName() const
 {
-	return QString::fromUtf8("Работник %1 %2.%3.").
-				arg(m_familyName->text()).
-				arg(m_name->text().left(1)).
-				arg(m_patronymic->text().left(1));
+	QString result = "Работник";
+
+	if(!m_familyName->text().simplified().isEmpty())
+	{
+		result += " " + m_familyName->text();
+
+		if(!m_name->text().trimmed().isEmpty() &&
+		   !m_patronymic->text().trimmed().isEmpty())
+		{
+			result += " " + m_name->text().trimmed().left(1) + "." +
+					  m_patronymic->text().trimmed().left(1) + ".";
+
+		}
+	}
+
+	return result;
 }
 
 
@@ -82,22 +98,31 @@ bool StaffEditWidget::canSave(QString &errorDescription) const
 void StaffEditWidget::save()
 {
 	QSqlQuery q;
-	q.prepare(" UPDATE Staff SET "
-			  " familyName = :familyName, "
-			  " name = :name, "
-			  " patronymic = :patronymic, "
-			  " birthday = :birthday, "
-			  " specialization = :specialization "
-			  " WHERE id = :id ");
+
+	if(m_staffId == InvalidId)
+	{
+		q.prepare(" INSERT INTO Staff "
+				  " ( familyName,  name,  patronymic,  birthday,  specialization) VALUES"
+				  " (:familyName, :name, :patronymic, :birthday, :specialization) ");
+	}
+	else
+	{
+		q.prepare(" UPDATE Staff SET "
+				  " familyName = :familyName, "
+				  " name = :name, "
+				  " patronymic = :patronymic, "
+				  " birthday = :birthday, "
+				  " specialization = :specialization "
+				  " WHERE id = :id ");
+		q.bindValue(":id", m_staffId);
+	}
 	q.bindValue(":familyName", nullIfEmpty(m_familyName->text().simplified()));
 	q.bindValue(":name", nullIfEmpty(m_name->text().simplified()));
 	q.bindValue(":patronymic", nullIfEmpty(m_patronymic->text().simplified()));
 	q.bindValue(":birthday", m_birthDay->date());
 	q.bindValue(":specialization", m_specialization->text().simplified());
-	q.bindValue(":id", m_staffId);
 	q.exec();
 	checkQuery(q);
-
 
 	emit saved();
 }
