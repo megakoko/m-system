21dd6fedb4ad2e0ce17923a40babcf57b3b9b67c
Andy <12209839+megakoko@users.noreply.github.com>
2012-02-01 11:01:12 +0000

    Добавил скрипт для генерации случайный шестнадцатеричных значений.

---
+++ src/resources/randomhexarray.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Script for generating random 2-byte hex numbers.
+# Required length of the array should be passed as the argument
+# to this script.
+
+from sys import argv
+from random import choice
+
+if len(argv) > 1:
+	# Codes for characters '0'...'9', 'A'...'F'.
+	integers = range(ord('0'), ord('9')+1) + range(ord('A'), ord('F')+1)
+
+	# Length of the hex array
+	length = 0;
+	try:
+		length = int(argv[1])
+	except:
+		print 'Wrong array length:', argv[1]
+		exit(1)
+	
+	# Print array 4 items per row.
+	for i in range(0, length):
+		print '0x' + chr(choice(integers)) + chr(choice(integers)) + ',',
+		if i % 4 == 3:
+			print ''
+			
