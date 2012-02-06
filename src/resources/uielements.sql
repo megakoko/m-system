deed3a638fb2dc8111cbdea39032b0f346902124
megadestroyer <megadestroyer@5efcf63a-43f3-4715-9d6f-55a9d6060040>
2012-02-06 14:46:21 +0000

    Добавил реальные данные первичного осмотра, секции "Анамнез жизни",  "Общий осмотр", "Опорно-двигательный аппарат", "Шея".

--- src/resources/uielements.sql
+++ src/resources/uielements.sql
@@ -7,38 +7,310 @@ UNION SELECT 'textedit'
 UNION SELECT 'spinbox';
 
 
+INSERT INTO UiElement(id, textId, parentId, availableForSexId, typeId, label)
+SELECT 01, 'main', NULL, CAST(NULL AS "numeric"), 'container', NULL;
+
+
 -- Элементы интерфейса.
 INSERT INTO UiElement(id, textId, parentId, availableForSexId, typeId, label)
-SELECT 9,  'main', NULL, CAST(NULL AS "numeric"), 'container', 'asd' UNION
-SELECT 10, 'top1', 'main', NULL, 'container', 'top item 1' UNION
-SELECT 20, 'combo1', 'top1', NULL, 'combobox', 'combo box 1' UNION
-SELECT 30, 'combo2', 'top1', NULL, 'combobox', 'combo box 2' UNION 
-SELECT 40, 'line1', 'top1', NULL, 'lineedit', 'line edit 1' UNION
-SELECT 50, 'top2', 'main', NULL, 'container', 'top container 2' UNION
-SELECT 60, 'combo3', 'top2', NULL, 'combobox', 'combo box 3 inside container 2';
-
+SELECT 10, 'PA', 'main', CAST(NULL AS "numeric"), 'container', 'Анамнез жизни' UNION
+SELECT 20, 'PA_PD', 'PA', NULL, 'lineedit', 'Перенесённые заболевания' UNION
+SELECT 30, 'PA_TO', 'PA', NULL, 'lineedit', 'Травмы и операции' UNION
+SELECT 40, 'PA_VH', 'PA', NULL, 'lineedit', 'Вирусный гепатит' UNION
+SELECT 50, 'PA_Tu', 'PA', NULL, 'lineedit', 'Туберкулёз' UNION
+SELECT 60, 'PA_VD', 'PA', NULL, 'lineedit', 'Венерические заболевания' UNION
+SELECT 70, 'PA_PM', 'PA', NULL, 'lineedit', 'Сахарный диабет' UNION
+SELECT 80, 'PA_EA', 'PA', NULL, 'combobox', 'Эпидемиологический анамнез' UNION
+SELECT 90, 'PA_AA', 'PA', NULL, 'lineedit', 'Аллергологический анамнез' UNION
+SELECT 100, 'PA_HT', 'PA', NULL, 'lineedit', 'Гемотрансфузии в анамнезе' UNION
+SELECT 110, 'PA_UI', 'PA', NULL, 'combobox', 'Привычные интоксикации' UNION
+SELECT 120, 'PA_AH', 'PA', NULL, 'lineedit', 'Наследственность' UNION
+SELECT 130, 'PA_FS', 'PA', NULL, 'lineedit', 'Семейное положение' UNION
+SELECT 140, 'PA_UP', 'PA', NULL, 'combobox', 'Условия проживания' UNION
+SELECT 150, 'PA_OGA', 'PA', NULL, 'lineedit', 'Акушерско-гинекологический анамнез' UNION
+SELECT 160, 'PA_M', 'PA', NULL, 'lineedit', 'Menses' UNION
+SELECT 170, 'PA_exp', 'PA', NULL, 'combobox', 'Страховой анамнез' UNION
+SELECT 180, 'PAd', 'PA', NULL, 'lineedit', 'Дополнительно';
 
 INSERT INTO UiElementEnums(id, uiElementTextId, value)
-SELECT 1, 'combo1', 'AQ40I4DL91ZGNH5JEHSM' UNION
-SELECT 2, 'combo1', 'KQZ36BEKAVP8O71BUGFD' UNION
-SELECT 3, 'combo1', 'M1QBNWE37F52SGKP3H0Q' UNION
-SELECT 4, 'combo1', 'BD59HWUR1ZQGNM4MA7VM' UNION
-SELECT 5, 'combo2', 'TGWGJ1ZRL56F10WMC3O2' UNION
-SELECT 6, 'combo2', 'SS83NNXVCOD092IM9BLP' UNION
-SELECT 7, 'combo2', 'H9TZB6NFAXOJQWD51JF6' UNION
-SELECT 8, 'combo2', 'J92ITAG0AXQZZ5VFLJPO' UNION
-SELECT 9, 'combo2', 'IA1ELV1BMV74NLR9L8Z3' UNION
-SELECT 10, 'combo2', 'MU21EKL5SCCBVK36VZAU' UNION
-SELECT 11, 'combo2', '5QQVV1YEE2FITFFLBXPM' UNION
-SELECT 12, 'combo2', 'H7FHNOQWYNZUSWA31HNT' UNION
-SELECT 13, 'combo3', '7PCGFLKGBRXDCH0H3Q4A' UNION
-SELECT 14, 'combo3', 'S7GKP31QQV2M3C2IGZLV' UNION
-SELECT 15, 'combo3', 'D37FS0BN63XR9ZG3X40U' UNION
-SELECT 16, 'combo3', 'GD3HJYHEFTUFT26FA6OQ' UNION
-SELECT 17, 'combo3', '2HE81SP1UVOHY7L3Q9LK' UNION
-SELECT 18, 'combo3', 'LROA62F9R2RJ4FNUVEZ3' UNION
-SELECT 19, 'combo3', 'ZEKMZBKZ8JUHONW12YKZ' UNION
-SELECT 20, 'combo3', 'UKHGWKSWBHJEWYK8Q4YG' UNION
-SELECT 21, 'combo3', 'BZ2V46JK8NLBH6SW8XIB';
+SELECT 10, 'PA_EA', 'контакты с инфекционными больными отрицает' UNION
+SELECT 20, 'PA_EA', 'употребление недоброкачественных продуктов отрицает' UNION
+SELECT 30, 'PA_UI', 'отрицает' UNION
+SELECT 40, 'PA_UI', 'курит' UNION
+SELECT 50, 'PA_UI', 'употребляет алкоголь' UNION
+SELECT 60, 'PA_UI', 'употребляет наркотические вещества' UNION
+SELECT 70, 'PA_UP', 'проживает в изолированной квартире со всеми удобствами' UNION
+SELECT 80, 'PA_UP', 'проживает в собственном доме со всеми удобствами' UNION
+SELECT 90, 'PA_UP', 'проживает в собственном доме с частичными удобствами' UNION
+SELECT 100, 'PA_UP', 'проживает в собственном доме без удобств' UNION
+SELECT 110, 'PA_UP', 'проживает в общежитии' UNION
+SELECT 120, 'PA_UP', 'не имеет постоянного места жительства' UNION
+SELECT 130, 'PA_exp', 'работающий' UNION
+SELECT 140, 'PA_exp', 'неработающий' UNION
+SELECT 150, 'PA_exp', 'инвалид III группы' UNION
+SELECT 160, 'PA_exp', 'инвалид II группы' UNION
+SELECT 170, 'PA_exp', 'инвалид I группы' UNION
+SELECT 180, 'PA_exp', 'открытого больничного листа нет' UNION
+SELECT 190, 'PA_exp', 'открыт больничный лист № с' UNION
+SELECT 200, 'PA_exp', 'КЭК проводилась';
 
+
+
+INSERT INTO UiElement(id, textId, parentId, availableForSexId, typeId, label)
+SELECT 1010, 'USall', 'main', CAST(NULL AS "numeric"), 'container', 'Общий осмотр' UNION
+SELECT 1020, 'US_GC', 'USall', NULL, 'combobox', 'Общее состояние' UNION
+SELECT 1030, 'US_C', 'USall', NULL, 'combobox', 'Сознание' UNION
+SELECT 1040, 'US_PP', 'USall', NULL, 'combobox', 'Положение пациента' UNION
+SELECT 1050, 'US_Look', 'USall', NULL, 'combobox', 'Выражение лица' UNION
+SELECT 1060, 'US_Gait', 'USall', NULL, 'combobox', 'Походка' UNION
+SELECT 1070, 'US_BB', 'USall', NULL, 'combobox', 'Телосложение' UNION
+--SELECT 1080, 'US_CT', 'USall', NULL, 'combobox', 'Конституциональный тип' UNION
+SELECT 1090, 'US_MB', 'USall', NULL, 'lineedit', 'Масса тела, кг' UNION
+SELECT 1100, 'US_Stature', 'USall', NULL, 'lineedit', 'Рост, см' UNION
+SELECT 1110, 'US_TB', 'USall', NULL, 'lineedit', 'Температура тела, °C' UNION
+SELECT 1120, 'US_Coverlet', 'USall', NULL, 'combobox', 'Кожные покровы' UNION
+SELECT 1130, 'US_MV', 'USall', NULL, 'combobox', 'Видимые слизистые' UNION
+SELECT 1140, 'US_SF', 'USall', NULL, 'combobox', 'Подкожно-жировая клетчатка' UNION
+SELECT 1150, 'US_E', 'USall', NULL, 'lineedit', 'Отеки' UNION
+SELECT 1160, 'US_S', 'USall', NULL, 'container', 'Придатки кожи' UNION
+--SELECT 1170, 'US_Sd', 'USall', NULL, 'combobox', '' UNION
+SELECT 1180, 'US_SHt', 'US_S', NULL, 'lineedit', 'Волосы' UNION
+SELECT 1190, 'US_SNt', 'US_S', NULL, 'lineedit', 'Ногти' UNION
+SELECT 1200, 'US_SMt', 'US_S', NULL, 'lineedit', 'Молочные железы' UNION
+SELECT 1210, 'US_Gd', 'USall', NULL, 'combobox', 'Лимфатические узлы' UNION
+SELECT 1220, 'US_G', 'USall', NULL, 'container', 'Лимфатические узлы' UNION
+SELECT 1230, 'US_Gsm', 'US_G', NULL, 'combobox', 'подчелюстные' UNION
+SELECT 1240, 'US_Gce', 'US_G', NULL, 'combobox', 'шейные' UNION
+SELECT 1250, 'US_Gsc', 'US_G', NULL, 'combobox', 'надключичные' UNION
+SELECT 1260, 'US_Gbc', 'US_G', NULL, 'combobox', 'подключичные' UNION
+SELECT 1270, 'US_Gsa', 'US_G', NULL, 'combobox', 'подмышечные' UNION
+SELECT 1280, 'US_Glo', 'US_G', NULL, 'combobox', 'локтевые' UNION
+SELECT 1290, 'US_Gpa', 'US_G', NULL, 'combobox', 'паховые' UNION
+SELECT 1300, 'US_Gbe', 'US_G', NULL, 'combobox', 'бедренные' UNION
+SELECT 1310, 'USd', 'USall', NULL, 'lineedit', 'Дополнительно';
+
+INSERT INTO UiElementEnums(id, uiElementTextId, value)
+SELECT 1010, 'US_GC', 'удовлетворительное' UNION
+SELECT 1020, 'US_GC', 'средней тяжести' UNION
+SELECT 1030, 'US_GC', 'тяжёлое' UNION
+SELECT 1040, 'US_GC', 'крайне тяжёлое' UNION
+SELECT 1050, 'US_GC', 'агональное' UNION
+SELECT 1060, 'US_C', 'ясное' UNION
+SELECT 1070, 'US_C', 'угнетенное' UNION
+SELECT 1080, 'US_C', 'пациент в состоянии ступора' UNION
+SELECT 1090, 'US_C', 'пациент в состоянии сопора' UNION
+SELECT 1100, 'US_C', 'пациент в состоянии оглушенности' UNION
+SELECT 1110, 'US_C', 'пациент в состоянии загруженности' UNION
+SELECT 1120, 'US_C', 'пациент в состоянии психомоторного возбуждения' UNION
+SELECT 1130, 'US_C', 'кома I' UNION
+SELECT 1140, 'US_C', 'кома II' UNION
+SELECT 1150, 'US_C', 'кома III' UNION
+SELECT 1160, 'US_C', 'отсутствует' UNION
+SELECT 1170, 'US_PP', 'активное' UNION
+SELECT 1180, 'US_PP', 'пассивное' UNION
+SELECT 1190, 'US_PP', 'вынужденное' UNION
+SELECT 1200, 'US_Look', 'возбужденное' UNION
+SELECT 1210, 'US_Look', 'спокойное' UNION
+SELECT 1220, 'US_Look', 'безразличное' UNION
+SELECT 1230, 'US_Look', 'страдальческое' UNION
+SELECT 1240, 'US_Look', 'маскообразное' UNION
+SELECT 1250, 'US_Gait', 'передвигается свободно' UNION
+SELECT 1260, 'US_Gait', 'при ходьбе хромает' UNION
+SELECT 1270, 'US_Gait', 'при ходьбе пользуется костылями' UNION
+SELECT 1280, 'US_Gait', 'при ходьбе пользуется палкой' UNION
+SELECT 1290, 'US_BB', 'правильное' UNION
+SELECT 1300, 'US_BB', 'неправильное' UNION
+--SELECT 1310, 'US_CT', 'астенический' UNION
+--SELECT 1320, 'US_CT', 'нормостенический' UNION
+--SELECT 1330, 'US_CT', 'гиперстенический' UNION
+SELECT 1340, 'US_Coverlet', 'физиологической окраски' UNION
+SELECT 1350, 'US_Coverlet', 'гиперемированные' UNION
+SELECT 1360, 'US_Coverlet', 'бледноватые' UNION
+SELECT 1370, 'US_Coverlet', 'бледные' UNION
+SELECT 1380, 'US_Coverlet', 'субиктеричные' UNION
+SELECT 1390, 'US_Coverlet', 'иктеричные' UNION
+SELECT 1400, 'US_Coverlet', 'цианотичные' UNION
+SELECT 1410, 'US_Coverlet', 'землистого оттенка' UNION
+SELECT 1420, 'US_Coverlet', 'сухие' UNION
+SELECT 1430, 'US_Coverlet', 'влажные' UNION
+SELECT 1440, 'US_Coverlet', 'эластичные' UNION
+SELECT 1450, 'US_Coverlet', 'без сыпи' UNION
+SELECT 1460, 'US_Coverlet', 'тургор снижен' UNION
+SELECT 1470, 'US_MV', 'физиологической окраски' UNION
+SELECT 1480, 'US_MV', 'гиперемированные' UNION
+SELECT 1490, 'US_MV', 'бледноватые' UNION
+SELECT 1500, 'US_MV', 'бледные' UNION
+SELECT 1510, 'US_MV', 'субиктеричные' UNION
+SELECT 1520, 'US_MV', 'иктеричные' UNION
+SELECT 1530, 'US_MV', 'цианотичные' UNION
+SELECT 1540, 'US_MV', 'землистого оттенка' UNION
+SELECT 1550, 'US_MV', 'сухие' UNION
+SELECT 1560, 'US_MV', 'влажные' UNION
+SELECT 1570, 'US_MV', 'эластичные' UNION
+SELECT 1580, 'US_SF', 'развита слабо' UNION
+SELECT 1590, 'US_SF', 'развита умеренно' UNION
+SELECT 1600, 'US_SF', 'развита чрезмерно' UNION
+--SELECT 1610, 'US_Sd', 'Придатки кожи без особенностей' UNION
+SELECT 1620, 'US_Gd', 'без особенностей' UNION
+SELECT 1630, 'US_Gd', 'не увеличены' UNION
+SELECT 1640, 'US_Gd', 'подвижные' UNION
+SELECT 1650, 'US_Gd', 'не спаяны с кожей' UNION
+SELECT 1660, 'US_Gd', 'безболезненные' UNION
+SELECT 1670, 'US_Gd', 'увеличены' UNION
+SELECT 1680, 'US_Gd', 'неподвижные' UNION
+SELECT 1690, 'US_Gd', 'спаяны с кожей' UNION
+SELECT 1700, 'US_Gd', 'болезненные' UNION
+SELECT 1710, 'US_Gd', 'кожа над л/у гиперемирована' UNION
+SELECT 1720, 'US_Gd', 'наблюдается локальная гипертермия в области узлов' UNION
+SELECT 1730, 'US_Gsm', 'не изменены' UNION
+SELECT 1740, 'US_Gsm', 'не увеличены' UNION
+SELECT 1750, 'US_Gsm', 'подвижные' UNION
+SELECT 1760, 'US_Gsm', 'не спаяны с кожей' UNION
+SELECT 1770, 'US_Gsm', 'безболезненные' UNION
+SELECT 1780, 'US_Gsm', 'увеличены' UNION
+SELECT 1790, 'US_Gsm', 'неподвижные' UNION
+SELECT 1800, 'US_Gsm', 'спаяны с кожей' UNION
+SELECT 1810, 'US_Gsm', 'болезненные' UNION
+SELECT 1820, 'US_Gsm', 'кожа над л/у гиперемирована' UNION
+SELECT 1830, 'US_Gsm', 'наблюдается локальная гипертермия в области узлов' UNION
+SELECT 1840, 'US_Gce', 'не изменены' UNION
+SELECT 1850, 'US_Gce', 'не увеличены' UNION
+SELECT 1860, 'US_Gce', 'подвижные' UNION
+SELECT 1870, 'US_Gce', 'не спаяны с кожей' UNION
+SELECT 1880, 'US_Gce', 'безболезненные' UNION
+SELECT 1890, 'US_Gce', 'увеличены' UNION
+SELECT 1900, 'US_Gce', 'неподвижные' UNION
+SELECT 1910, 'US_Gce', 'спаяны с кожей' UNION
+SELECT 1920, 'US_Gce', 'болезненные' UNION
+SELECT 1930, 'US_Gce', 'кожа над л/у гиперемирована' UNION
+SELECT 1940, 'US_Gce', 'наблюдается локальная гипертермия в области узлов' UNION
+SELECT 1950, 'US_Gsc', 'не изменены' UNION
+SELECT 1960, 'US_Gsc', 'не увеличены' UNION
+SELECT 1970, 'US_Gsc', 'подвижные' UNION
+SELECT 1980, 'US_Gsc', 'не спаяны с кожей' UNION
+SELECT 1990, 'US_Gsc', 'безболезненные' UNION
+SELECT 2000, 'US_Gsc', 'увеличены' UNION
+SELECT 2010, 'US_Gsc', 'неподвижные' UNION
+SELECT 2020, 'US_Gsc', 'спаяны с кожей' UNION
+SELECT 2030, 'US_Gsc', 'болезненные' UNION
+SELECT 2040, 'US_Gsc', 'кожа над л/у гиперемирована' UNION
+SELECT 2050, 'US_Gsc', 'наблюдается локальная гипертермия в области узлов' UNION
+SELECT 2060, 'US_Gbc', 'не изменены' UNION
+SELECT 2070, 'US_Gbc', 'не увеличены' UNION
+SELECT 2080, 'US_Gbc', 'подвижные' UNION
+SELECT 2090, 'US_Gbc', 'не спаяны с кожей' UNION
+SELECT 2100, 'US_Gbc', 'безболезненные' UNION
+SELECT 2110, 'US_Gbc', 'увеличены' UNION
+SELECT 2120, 'US_Gbc', 'неподвижные' UNION
+SELECT 2130, 'US_Gbc', 'спаяны с кожей' UNION
+SELECT 2140, 'US_Gbc', 'болезненные' UNION
+SELECT 2150, 'US_Gbc', 'кожа над л/у гиперемирована' UNION
+SELECT 2160, 'US_Gbc', 'наблюдается локальная гипертермия в области узлов' UNION
+SELECT 2170, 'US_Gsa', 'не изменены' UNION
+SELECT 2180, 'US_Gsa', 'не увеличены' UNION
+SELECT 2190, 'US_Gsa', 'подвижные' UNION
+SELECT 2200, 'US_Gsa', 'не спаяны с кожей' UNION
+SELECT 2210, 'US_Gsa', 'безболезненные' UNION
+SELECT 2220, 'US_Gsa', 'увеличены' UNION
+SELECT 2230, 'US_Gsa', 'неподвижные' UNION
+SELECT 2240, 'US_Gsa', 'спаяны с кожей' UNION
+SELECT 2250, 'US_Gsa', 'болезненные' UNION
+SELECT 2260, 'US_Gsa', 'кожа над л/у гиперемирована' UNION
+SELECT 2270, 'US_Gsa', 'наблюдается локальная гипертермия в области узлов' UNION
+SELECT 2280, 'US_Glo', 'не изменены' UNION
+SELECT 2290, 'US_Glo', 'не увеличены' UNION
+SELECT 2300, 'US_Glo', 'подвижные' UNION
+SELECT 2310, 'US_Glo', 'не спаяны с кожей' UNION
+SELECT 2320, 'US_Glo', 'безболезненные' UNION
+SELECT 2330, 'US_Glo', 'увеличены' UNION
+SELECT 2340, 'US_Glo', 'неподвижные' UNION
+SELECT 2350, 'US_Glo', 'спаяны с кожей' UNION
+SELECT 2360, 'US_Glo', 'болезненные' UNION
+SELECT 2370, 'US_Glo', 'кожа над л/у гиперемирована' UNION
+SELECT 2380, 'US_Glo', 'наблюдается локальная гипертермия в области узлов' UNION
+SELECT 2390, 'US_Gpa', 'не изменены' UNION
+SELECT 2400, 'US_Gpa', 'не увеличены' UNION
+SELECT 2410, 'US_Gpa', 'подвижные' UNION
+SELECT 2420, 'US_Gpa', 'не спаяны с кожей' UNION
+SELECT 2430, 'US_Gpa', 'безболезненные' UNION
+SELECT 2440, 'US_Gpa', 'увеличены' UNION
+SELECT 2450, 'US_Gpa', 'неподвижные' UNION
+SELECT 2460, 'US_Gpa', 'спаяны с кожей' UNION
+SELECT 2470, 'US_Gpa', 'болезненные' UNION
+SELECT 2480, 'US_Gpa', 'кожа над л/у гиперемирована' UNION
+SELECT 2490, 'US_Gpa', 'наблюдается локальная гипертермия в области узлов' UNION
+SELECT 2500, 'US_Gbe', 'не изменены' UNION
+SELECT 2510, 'US_Gbe', 'не увеличены' UNION
+SELECT 2520, 'US_Gbe', 'подвижные' UNION
+SELECT 2530, 'US_Gbe', 'не спаяны с кожей' UNION
+SELECT 2540, 'US_Gbe', 'безболезненные' UNION
+SELECT 2550, 'US_Gbe', 'увеличены' UNION
+SELECT 2560, 'US_Gbe', 'неподвижные' UNION
+SELECT 2570, 'US_Gbe', 'спаяны с кожей' UNION
+SELECT 2580, 'US_Gbe', 'болезненные' UNION
+SELECT 2590, 'US_Gbe', 'кожа над л/у гиперемирована' UNION
+SELECT 2600, 'US_Gbe', 'наблюдается локальная гипертермия в области узлов';
+
+
+
+INSERT INTO UiElement(id, textId, parentId, availableForSexId, typeId, label)
+SELECT 3010, 'MSSall', 'main', CAST(NULL AS "numeric"), 'container', 'Опорно-двигательный аппарат' UNION
+SELECT 3020, 'MSSS', 'MSSall', NULL, 'lineedit', 'Костные структуры' UNION
+SELECT 3030, 'MSSM', 'MSSall', NULL, 'container', 'Мышечная система' UNION
+SELECT 3040, 'NS_dva', 'MSSM', NULL, 'lineedit', 'Атрофия мышц' UNION
+SELECT 3050, 'NS_dvg', 'MSSM', NULL, 'lineedit', 'Истинная гипертрофия мышц' UNION
+SELECT 3060, 'NS_dvp', 'MSSM', NULL, 'lineedit', 'Псевдогипертрофии' UNION
+SELECT 3070, 'NS_dvf', 'MSSM', NULL, 'lineedit', 'Фибриллярные и фасцикулярные' UNION
+SELECT 3080, 'NS_dvr', 'MSSM', NULL, 'lineedit', 'Ретракции и контрактуры' UNION
+SELECT 3090, 'NS_dvo', 'MSSM', NULL, 'lineedit', 'Объем движений в конечностях' UNION
+SELECT 3100, 'NS_dvs', 'MSSM', NULL, 'lineedit', 'Сила мышц дистальных и' UNION
+SELECT 3110, 'NS_dvt', 'MSSM', NULL, 'lineedit', 'Тонус мышц' UNION
+SELECT 3120, 'MSSJ', 'MSSall', NULL, 'container', 'Суставы' UNION
+SELECT 3130, 'MSSJ_1', 'MSSJ', NULL, 'lineedit', 'Конфигурация' UNION
+SELECT 3140, 'MSSJ_2', 'MSSJ', NULL, 'lineedit', 'Болезненность при пальпации' UNION
+SELECT 3150, 'MSSJ_3', 'MSSJ', NULL, 'lineedit', 'Активные движения' UNION
+SELECT 3160, 'MSSJ_4', 'MSSJ', NULL, 'lineedit', 'Пассивные движения' UNION
+SELECT 3170, 'MSSd', 'MSSall', NULL, 'lineedit', 'Дополнительно';
+
+--INSERT INTO UiElementEnums(id, uiElementTextId, value)
+--SELECT 3010, 'MSSM', 'без явных признаков патологии' UNION
+--SELECT 3020, 'MSSJ', 'без явных признаков патологии';
+
+
+
+INSERT INTO UiElement(id, textId, parentId, availableForSexId, typeId, label)
+SELECT 4010, 'Neckall', 'main', CAST(NULL AS "numeric"), 'container', 'Шея' UNION
+SELECT 4020, 'NeckF', 'Neckall', NULL, 'lineedit', 'Форма шеи' UNION
+SELECT 4030, 'NeckV', 'Neckall', NULL, 'combobox', 'Голос' UNION
+SELECT 4040, 'NeckTG', 'Neckall', NULL, 'container', 'Щитовидная железа' UNION
+SELECT 4050, 'NeckTGS', 'NeckTG', NULL, 'combobox', 'Размеры' UNION
+SELECT 4060, 'NeckTGM', 'NeckTG', NULL, 'combobox', 'Подвижность' UNION
+SELECT 4070, 'NeckTGP', 'NeckTG', NULL, 'combobox', 'Болезненность при пальпации' UNION
+SELECT 4080, 'NeckTGCS', 'NeckTG', NULL, 'combobox', 'Консистенция' UNION
+SELECT 4090, 'NeckTGSU', 'NeckTG', NULL, 'combobox', 'Поверхность' UNION
+SELECT 4100, 'NeckD', 'Neckall', NULL, 'lineedit', 'Дополнительно';
+
+INSERT INTO UiElementEnums(id, uiElementTextId, value)
+SELECT 4010, 'NeckV', 'чёткий' UNION
+SELECT 4020, 'NeckV', 'осиплый' UNION
+SELECT 4030, 'NeckV', 'хриплый' UNION
+SELECT 4040, 'NeckV', 'наблюдается афазия' UNION
+SELECT 4050, 'NeckTG', 'не увеличена' UNION
+SELECT 4060, 'NeckTG', 'увеличение I степени' UNION
+SELECT 4070, 'NeckTG', 'увеличение II степени' UNION
+SELECT 4080, 'NeckTG', 'увеличение III степени' UNION
+SELECT 4090, 'NeckTG', 'увеличение IV степени' UNION
+SELECT 4100, 'NeckTGS', 'не увеличена' UNION
+SELECT 4110, 'NeckTGS', 'увеличение I степени' UNION
+SELECT 4120, 'NeckTGS', 'увеличение II степени' UNION
+SELECT 4130, 'NeckTGS', 'увеличение III степени' UNION
+SELECT 4140, 'NeckTGS', 'увеличение IV степени' UNION
+SELECT 4150, 'NeckTGM', 'при глотании смещается' UNION
+SELECT 4160, 'NeckTGM', 'при глотании не смещается' UNION
+SELECT 4170, 'NeckTGP', 'при пальпации безболезненная' UNION
+SELECT 4180, 'NeckTGP', 'при пальпации болезненная' UNION
+SELECT 4190, 'NeckTGCS', 'мягкая' UNION
+SELECT 4200, 'NeckTGCS', 'плотная' UNION
+SELECT 4210, 'NeckTGSU', 'поверхность гладкая' UNION
+SELECT 4220, 'NeckTGSU', 'поверхность бугристая';
 
