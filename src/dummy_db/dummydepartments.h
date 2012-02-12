58bea21edb32beefc6c54b4fe7529dce20b09fef
Andy <12209839+megakoko@users.noreply.github.com>
2012-02-12 07:25:36 +0000

    Рефакторинг: выделил создание тестовых отделений/работников в отдельный класс.

---
+++ src/dummy_db/dummydepartments.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+#include "dummydata.h"
+
+class DummyDepartments
+{
+public:
+	DummyDepartments(DummyDataPtr dummyData);
+
+	void createStaff(const int count);
+	void createDepartments();
+
+private:
+	/// Возвращает идентификаторы всех должностей из базы данных.
+	QVariantList positionIdsFromDatabase() const;
+	/// Возвращает идентификаторы всех сотрудников из базы данных.
+	QVariantList staffIdsFromDatabase() const;
+
+
+	/// Создает тестовые должности в БД.
+	void createPositions();
+	/// Создает записи в таблице DepartmentStaffPosition.
+	/// (то есть заполняет отделение \a depaertmentId случайными сотрудниками из списка
+	/// \a staffIds с должностями из списка \a staffIds.
+	void createDepartmentStaffPositions(const QVariant departmentId,
+										const QVariant headOfDepartmentId,
+										const QVariantList& staffIds,
+										const QVariantList& positionIds);
+
+	DummyDataPtr m_dummyData;
+
+};
