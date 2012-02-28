ec3d00524d7b39a2be79637bc36056446b88490f
megadestroyer <megadestroyer@5efcf63a-43f3-4715-9d6f-55a9d6060040>
2012-02-28 16:55:57 +0000

    Добавил несколько правил для диагностики с выдуманными вероятностями.

---
+++ src/resources/diagnosis_rules.sql
@@ -0,0 +1,33 @@
+INSERT INTO DsRule(diseaseText, diseaseProbability) VALUES('Пневмония', 0.00518);
+INSERT INTO DsRule(diseaseText, diseaseProbability) VALUES('Острый бронхит', 0.004);
+INSERT INTO DsRule(diseaseText, diseaseProbability) VALUES('Обструктивный бронхит', 0.003);
+INSERT INTO DsRule(diseaseText, diseaseProbability) VALUES('Острый трахеит', 0.005);
+INSERT INTO DsRule(diseaseText, diseaseProbability) VALUES('Острый фарингит', 0.006);
+
+
+INSERT INTO dsruleitem VALUES (1, 1, 6015, 2, NULL, NULL, NULL, 6001, 0.3, 0.1);
+INSERT INTO dsruleitem VALUES (2, 1, 1110, 4, NULL, 38, NULL, NULL, 0.4, 0.2);
+INSERT INTO dsruleitem VALUES (3, 1, 6070, 1, NULL, NULL, NULL, 6147, 0.3, 0.3);
+INSERT INTO dsruleitem VALUES (4, 1, 6080, 1, NULL, NULL, NULL, 6182, 0.5, 0.2);
+INSERT INTO dsruleitem VALUES (5, 1, 6090, 1, NULL, NULL, NULL, 6186, 0.5, 0.2);
+INSERT INTO dsruleitem VALUES (6, 2, 6015, 2, NULL, NULL, NULL, 6001, 0.3, 0.1);
+INSERT INTO dsruleitem VALUES (7, 2, 1110, 4, NULL, 37, NULL, NULL, 0.4, 0.2);
+INSERT INTO dsruleitem VALUES (8, 2, 6080, 1, NULL, NULL, NULL, 6170, 0.3, 0.3);
+INSERT INTO dsruleitem VALUES (9, 2, 6090, 1, NULL, NULL, NULL, 6186, 0.4, 0.1);
+INSERT INTO dsruleitem VALUES (10, 3, 6015, 1, NULL, NULL, NULL, 6002, 0.4, 0.1);
+INSERT INTO dsruleitem VALUES (11, 3, 6070, 1, NULL, NULL, NULL, 6146, 0.4, 0.2);
+INSERT INTO dsruleitem VALUES (12, 3, 6080, 1, NULL, NULL, NULL, 6182, 0.5, 0.1);
+INSERT INTO dsruleitem VALUES (13, 3, 6090, 1, NULL, NULL, NULL, 6187, 0.6, 0.3);
+INSERT INTO dsruleitem VALUES (14, 4, 6015, 1, NULL, NULL, NULL, 6003, 0.4, 0.1);
+INSERT INTO dsruleitem VALUES (15, 4, 1110, 4, NULL, 37, NULL, NULL, 0.4, 0.1);
+INSERT INTO dsruleitem VALUES (16, 4, 6070, 1, NULL, NULL, NULL, 6145, 0.3, 0.4);
+INSERT INTO dsruleitem VALUES (17, 4, 6080, 1, NULL, NULL, NULL, 6150, 0.2, 0.1);
+INSERT INTO dsruleitem VALUES (18, 4, 6090, 1, NULL, NULL, NULL, 6185, 0.3, 0.3);
+INSERT INTO dsruleitem VALUES (19, 4, 6115, 1, NULL, NULL, NULL, 6301, 0.3, 0.1);
+INSERT INTO dsruleitem VALUES (20, 5, 6015, 1, NULL, NULL, NULL, 6004, 0.2, 0.2);
+INSERT INTO dsruleitem VALUES (21, 5, 1110, 4, NULL, 37, NULL, NULL, 0.1, 0.1);
+INSERT INTO dsruleitem VALUES (22, 5, 6070, 1, NULL, NULL, NULL, 6145, 0.1, 0.2);
+INSERT INTO dsruleitem VALUES (23, 5, 6080, 1, NULL, NULL, NULL, 6181, 0.4, 0.3);
+INSERT INTO dsruleitem VALUES (24, 5, 6090, 1, NULL, NULL, NULL, 6185, 0.3, 0.2);
+INSERT INTO dsruleitem VALUES (25, 5, 6116, 1, NULL, NULL, NULL, 6311, 0.5, 0.2);
+
