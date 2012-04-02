809622af930cafd734bb745a70c406e0cd54d101
megadestroyer <megadestroyer@5efcf63a-43f3-4715-9d6f-55a9d6060040>
2012-04-02 17:48:59 +0000

    Добавил поддержку КЛАДР в автодополнении полей. Пока без таблиц.

--- src/patients/addressdialog.h
+++ src/patients/addressdialog.h
@@ -4,6 +4,9 @@
 #include "address.h"
 
 
+class QSqlQueryModel;
+
+
 class AddressDialog : public QDialog, private Ui::AddressDialog
 {
 	Q_OBJECT
@@ -13,9 +16,30 @@ public:
 	Address address();
 
 private:
+	bool kladrIsAvailable() const;
+
+	static QString getCode(const QString& name, const int level, const QString& tableName, const QString &codeStart);
+
+	QString getRegionCode() const;
+	QString getCityCode(const QString& codeBeginning) const;
+	QString getStreetCode(const QString& codeBeginning) const;
+
+	static bool cityBelongsToRegion(const QString& cityCode, const QString& regionCode);
+	static bool streetBelongsToCity(const QString& streetCode, const QString& cityCode);
+
 	Address m_address;
+
+	QSqlQueryModel* m_regionModel;
+	QSqlQueryModel* m_cityModel;
+	QSqlQueryModel* m_streetModel;
+	QSqlQueryModel* m_houseModel;
 
 private slots:
 	void checkFields();
+
+	void updateRegionCompletition();
+	void updateCityCompletition();
+	void updateStreetCompletition();
+	void updateHouseCompletition();
 
 };
